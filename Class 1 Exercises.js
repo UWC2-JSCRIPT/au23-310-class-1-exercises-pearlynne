75c6992d7475e4927f750cb8f30fbb54715b003e
Pearlynne <chongpearlynnelh@gmail.com>
2023-10-11 17:39:29 -0700

    Update answers for Questions 1 and 2

--- Class 1 Exercises.js
+++ Class 1 Exercises.js
@@ -11,26 +11,30 @@ const LARGE_PIZZA_DIAMETER = 17;
 const SMALL_PIZZA_PRICE = 16.99;
 const LARGE_PIZZA_PRICE = 19.99;
 
-const smallPizzaArea = Math.PI * ((SMALL_PIZZA_DIAMETER / 2) ** 2);
-const largPizzaArea = Math.PI * ((LARGE_PIZZA_DIAMETER / 2) ** 2);
+const smallPizzaArea = Math.PI * ((SMALL_PIZZA_DIAMETER / 2) ** 2); // Math.pow is an alternative
+const largePizzaArea = Math.PI * ((LARGE_PIZZA_DIAMETER / 2) ** 2);
 
+console.log("The area of the small pizza is", smallPizzaArea)
+console.log("The area of the large pizza is", largePizzaArea)
 
 // 2. What is the cost per square inch of each pizza?
 
 const smallPizzaCostPerSqInch = SMALL_PIZZA_PRICE / smallPizzaArea;
 const largePizzaCostPerSqInch = LARGE_PIZZA_PRICE / largePizzaArea;
 
-console.log(smallPizzaArea)
-console.log(largPizzaArea)
+console.log("The cost per square inch of the small pizza is", smallPizzaCostPerSqInch)
+console.log("The cost per square inch of the large pizza is", largePizzaCostPerSqInch)
 
 // 3. Using the Math object, put together a code snippet
 // that allows you to draw a random card with a value
 // between 1 and 13 (assume ace is 1, jack is 11…)
 
 
+// Math.random gives fraction and between 0 and 1. 
+// so we have to use Math.ceil and we have to get highlest 13
+
 // 4. Draw 3 cards and use Math to determine the highest
 // card
-
 
 /**
  * ADDRESS LINE
